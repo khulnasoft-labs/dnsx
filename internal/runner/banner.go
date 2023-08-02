b155c45971f765129b6f9e979d6a1d986b5d9882
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-02 01:32:33 +0000

    first commit

---
+++ internal/runner/banner.go
@@ -0,0 +1,34 @@
+package runner
+
+import (
+	"github.com/khulnasoft-labs/gologger"
+	updateutils "github.com/khulnasoft-labs/utils/update"
+)
+
+const banner = `
+      _             __  __
+   __| | _ __   ___ \ \/ /
+  / _' || '_ \ / __| \  / 
+ | (_| || | | |\__ \ /  \ 
+  \__,_||_| |_||___//_/\_\
+`
+
+// Name
+const ToolName = `dnsx`
+
+// version is the current version of dnsx
+const version = `1.1.4`
+
+// showBanner is used to show the banner to the user
+func showBanner() {
+	gologger.Print().Msgf("%s\n", banner)
+	gologger.Print().Msgf("\t\tkhulnasoft-labs.io\n\n")
+}
+
+// GetUpdateCallback returns a callback function that updates dnsx
+func GetUpdateCallback() func() {
+	return func() {
+		showBanner()
+		updateutils.GetUpdateToolCallback("dnsx", version)()
+	}
+}
